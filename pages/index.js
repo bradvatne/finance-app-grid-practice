5161f029f601c7865cc57c76911fa15486dfff52
Brad Vatne <brad.vatne@gmail.com>
2021-02-28 13:39:54 +0800

    layout

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,37 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="app-container">
       <Head>
         <title>Finance Grid Practice</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid">
-        <div className="left">Finarium</div>
+        <div className="left">...</div>
         <div className="center">
-          <h1>Dashboard</h1>
+          <h1>...</h1>
           <p className="date">
-            <span className="grey">Showing for: </span>
-            <strong>20 Feb 2018 - 27 Feb 2018</strong>
+            <span className="grey">... </span>
           </p>
           <div className="icons">
-            d
+            <div className="icon-item">...</div>
+            <div className="icon-item">...</div>
+            <div className="icon-item">...</div>
+            <div className="icon-item">...</div>
+          </div>
+          <div className="balance">...</div>
+          <div className="expenses">...</div>
+          <div className="overview">
+            ...
           </div>
         </div>
-        <div className="right">right</div>
+
+        <div className="right">
+          <div className="user">
+            <div className="notifications">...</div>
+            <div className="account">...</div>
+          </div>
+        </div>
       </div>
     </div>
   );
